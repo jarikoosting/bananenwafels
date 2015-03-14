26cac0aec293f2d6ef917d47151fa3153d7f394c
Simon de Wit <simon@MacBook-Pro-van-Simon.local>
2015-03-14 18:27:34 +0100

    Made function to generate random coordinates

---
+++ randomShoot.py
@@ -0,0 +1,7 @@
+def randomShoot():
+    """
+    Function that generates random coordinations.
+    Needs randrange() from random module.
+    """
+    coord = (randrange(10), randrange(10))
+    return coord
